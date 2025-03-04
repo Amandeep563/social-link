b5459efbbf6dd26e089ec77e962cc535454c0c54
Amandeep <amandeepbollampalli@gmail.com>
2025-03-04 19:10:17 +0530

    Added all project

---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ProfileCard.css";
+
+function ProfileCard() {
+  return (
+    <div>
+      <h1>Hi, i'm creating social link lol</h1>
+    </div>
+  );
+}
+
+export default ProfileCard;
