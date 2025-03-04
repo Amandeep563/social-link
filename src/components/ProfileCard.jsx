126e2c990d50903b4768d2fa14f51e09c09cb073
Amandeep <amandeepbollampalli@gmail.com>
2025-03-04 21:20:36 +0530

    completed lol

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,10 +1,70 @@
 import React from "react";
+import soloLeveling from "../assets/solo-leveling.jpg";
+import dailyDev from "../assets/dailyDev.png";
+import codedex from "../assets/codedex.png";
 import "./ProfileCard.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 function ProfileCard() {
   return (
-    <div>
-      <h1>Hi, i'm creating social link lol</h1>
+    <div className="profile-container">
+      <div className="profile-card">
+        <img
+          src={soloLeveling}
+          alt="my profile photo"
+          className="profile-img"
+        ></img>
+        <h2 className="profile-name">Amandeep</h2>
+        <p className="profile-location">Hyderabad, India</p>
+        <p className="profile-bio">Full Stack Developer</p>
+        <div className="profile-links">
+          <a
+            href="https://github.com/Amandeep563"
+            target="_black"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} size="lg" />
+            <span> GitHub</span>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/amandeep-bollampalli/"
+            target="_black"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} size="lg" />
+            LinkedIn
+          </a>
+          <a
+            href="https://x.com/amandeep265"
+            target="_black"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faTwitter} size="lg" />
+            Twitter
+          </a>
+
+          <a
+            href="https://app.daily.dev/amandeep58"
+            target="_black"
+            rel="noopener noreferrer"
+          >
+            <img src={dailyDev} alt="daily.dev" width="24" height="24" />
+            daily.dev
+          </a>
+
+          <a
+            href="https://www.codedex.io/@amandeep5018"
+            target="_black"
+            rel="noopener noreferrer"
+          >
+            <img src={codedex} alt="daily.dev" width="24" height="24" />
+            Codédex
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
